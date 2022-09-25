e0cc1564031b73e22842138ff8905233683752b8
KalebPulley <pul21001@byui.edu>
2022-09-24 20:51:10 -0600

    lint

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,8 @@
 //return data
-function returnData(req, res){
-    const data = "I know Orian Pulley"
-    res.send(data);
+function returnData(req, res) {
+  const data = "I know Orian Pulley";
+  res.send(data);
 }
 module.exports = {
-    returnData,
+  returnData,
 };
