d195a00feb5afc7b5473c2c9e2c40e7cff59d00f
KalebPulley <pul21001@byui.edu>
2022-09-24 21:53:00 -0600

    url repaire

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,14 @@
 module.exports = {
   env: {
-    browser: true,
-    commonjs: true,
+    node: true,
+    jest: true,
     es2021: true,
   },
   extends: "eslint:recommended",
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
+    sourceType: "module"
   },
   rules: {},
 };
